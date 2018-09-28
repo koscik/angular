a9782a9be2f133aca364148f21bf4b4896ad83d6
Pawel Koscielinski <kosciks@gmail.com>
2018-09-28 10:31:05 +0200

    Angular material installed

--- WebApi/WebApi/AngularApp/angular-app/src/app/app.module.ts
+++ WebApi/WebApi/AngularApp/angular-app/src/app/app.module.ts
@@ -1,14 +1,27 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
+  exports:[
+    MatInputModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
