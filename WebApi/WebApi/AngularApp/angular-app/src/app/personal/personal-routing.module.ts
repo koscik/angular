bd96c9551fc182bce3532e6d4d9e0a35c17cf46d
Pawel Koscielinski <kosciks@gmail.com>
2018-10-09 09:28:27 +0200

    Application layout

--- WebApi/WebApi/AngularApp/angular-app/src/app/personal/personal-routing.module.ts
+++ WebApi/WebApi/AngularApp/angular-app/src/app/personal/personal-routing.module.ts
@@ -10,5 +10,5 @@ const routes: Routes = [
     imports: [RouterModule.forChild(routes)],
     exports: [RouterModule]
 })
-export class GamesRoutingModule {
+export class PersonalRoutingModule {
 }
--- WebApi/WebApi/AngularApp/angular-app/src/app/personal/personal.module.ts
+++ WebApi/WebApi/AngularApp/angular-app/src/app/personal/personal.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { SharedModule } from "../shared/shared.module";
 import { PersonalInfoComponent } from "./components/personal-info/personal-info.component";
+import { PersonalRoutingModule } from "./personal-routing.module";
 
 @NgModule({
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    PersonalRoutingModule
   ],
   declarations: [PersonalInfoComponent]
 })
